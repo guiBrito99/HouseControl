69a1926e3db934780578c93297f57ea9a1914eac
Guilherme <gsgdbrito@gmail.com>
2022-09-15 17:56:33 -0300

    Removed the toggle button and function and added two buttons that set all status to on or off

--- app/src/main/java/com/example/housecontrol/MainActivity.kt
+++ app/src/main/java/com/example/housecontrol/MainActivity.kt
@@ -11,7 +11,7 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private val clickListeners = arrayOfNulls<View.OnClickListener>(3)
-    private val buttons = arrayOfNulls<Button>(8)
+    private val buttons = arrayOfNulls<Button>(9)
     private val status = arrayOfNulls<TextView>(6)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         clickListeners[2] = View.OnClickListener{
-            toggleAll()
+            toggleAll(it)
         }
     }
 
@@ -74,9 +74,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun toggleAll(){
-        for(element in status)
-            reverseString(element)
+    private fun toggleAll(view: View){
+        when(view.id){
+            R.id.allOn_button -> setAll(R.string.on)
+            R.id.allOff_button -> setAll(R.string.off)
+        }
+    }
+
+    private fun setAll(statusString: Int){
+        for(element in status){
+            element?.setText(statusString)
+        }
     }
 
     private fun reverseString(status: TextView?){
@@ -96,17 +104,20 @@ class MainActivity : AppCompatActivity() {
         buttons[4] = findViewById(R.id.garage_button)
         buttons[5] = findViewById(R.id.television_button)
         buttons[6] = findViewById(R.id.artificialsun_button)
-        buttons[7] = findViewById(R.id.toggle_button)
+        buttons[7] = findViewById(R.id.allOn_button)
+        buttons[8] = findViewById(R.id.allOff_button)
 
         initializeButtonsWithClickListeners()
     }
 
     private fun initializeButtonsWithClickListeners(){
-        for( element in 0..5)
+        for(element in 0..5)
             buttons[element]?.setOnClickListener(clickListeners[0])
 
         buttons[6]?.setOnClickListener(clickListeners[1])
-        buttons[7]?.setOnClickListener(clickListeners[2])
+
+        for(element in 7..8)
+            buttons[element]?.setOnClickListener(clickListeners[2])
     }
 
 }
